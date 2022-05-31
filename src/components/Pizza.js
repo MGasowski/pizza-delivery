f8eabd05b993f1c103578c2383e2551649254a95
MGasowski <gasowski0marcin@gmail.com>
2022-05-31 10:42:26 +0200

    s01pt5

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
+
 const Pizza = ({ pizza }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [variant, setVariant] = useState("small");
+
   return (
-    <div>
+    <div className="m-5 shadow-lg p-3 mb-5 bg-white rounded">
       <h1>{pizza.name}</h1>
       <img
         src={pizza.image}
         alt=""
         className="img-fluid"
-        style={{ height: 200, width: 200 }}
+        style={{ height: 200, width: 200, objectFit: "contain" }}
       />
 
       <div className="flex-container">
-        <div className="w-100">
+        <div className="w-100 m-1">
           <p>Variants:</p>
-          <select>
+          <select
+            className="form-control"
+            value={variant}
+            onChange={(e) => setVariant(e.target.value)}
+          >
             {pizza.varients.map((variant) => {
               return (
                 <option key={variant} value={variant}>
@@ -22,9 +31,13 @@ const Pizza = ({ pizza }) => {
             })}
           </select>
         </div>
-        <div className="w-100">
+        <div className="w-100 m-1">
           <p>Quantity:</p>
-          <select>
+          <select
+            className="form-control"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          >
             {[...Array(10).keys()].map((value, index) => {
               return (
                 <option key={index} value={value + 1}>
@@ -33,6 +46,16 @@ const Pizza = ({ pizza }) => {
               );
             })}
           </select>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="m-1 w-100">
+          <h1 className="mt-1">
+            Price: ${pizza.prices[0][variant] * quantity}
+          </h1>
+        </div>
+        <div className="m-1 w-100">
+          <button className="btn"> ADD TO CART</button>
         </div>
       </div>
     </div>
